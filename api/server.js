0ee4fe38a7b4fc6519408298e020514af8bb0376
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-11 19:25:06 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -13,7 +13,9 @@ server.get('/', (req, res) => {
 })
 
 server.get('/api/dogs', (req, res) => {
-
+  // pull info from request (and validate it)
+  // interact with the databse
+  // send the client appropriate response
 })
 
 server.get('/api/dogs/:id', (req, res) => {
